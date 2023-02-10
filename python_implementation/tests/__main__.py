2bd78e66880c40e5e65530421dca155efde488c6
Ayo <ayooluwaadedipe@gmail.com>
2023-02-10 12:31:06 +0000

    Test heapify util

--- python_implementation/tests/__main__.py
+++ python_implementation/tests/__main__.py
@@ -1,6 +1,7 @@
 import unittest
 from utils.swap import swap
 from utils.calculate_parent_index import calculate_parent_index
+from utils.heapify import heapify
 
 class TestUtils(unittest.TestCase):
   def test_swap_util(self):
@@ -14,7 +15,9 @@ class TestUtils(unittest.TestCase):
     parent_index4 = calculate_parent_index(25, 3)
     parent_index5 = calculate_parent_index(24, 3)
     parent_index6 = calculate_parent_index(21, 3)
-    print(parent_index1, parent_index2, parent_index3, 'PARENT INDICESSS')
     self.assertTrue(parent_index1 == 3 and parent_index2 == 4 and parent_index3 == 5 and parent_index4 == 3 and parent_index5 == 3 and parent_index6 == 3)
+  def test_heapify(self):
+    test_heapified_array = heapify([None, 1, 9, 2, 3], 4, 4, 1)
+    self.assertTrue(test_heapified_array == [None, 3, 9, 2, 1])
 if __name__ == '__main__':
     unittest.main(verbosity=2)
