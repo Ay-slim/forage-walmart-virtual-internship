adc278f3c1c86f0c25808481396308560ce2325f
Ayo <ayooluwaadedipe@gmail.com>
2023-02-10 14:13:23 +0000

    Implement swap util in place

--- python_implementation/utils/swap.py
+++ python_implementation/utils/swap.py
@@ -1,5 +1,3 @@
 def swap(array, first_index, second_index):
-  temp = array[first_index]
-  array[first_index] = array[second_index]
-  array[second_index] = temp
+  (array[first_index], array[second_index]) = (array[second_index], array[first_index])
   return array
