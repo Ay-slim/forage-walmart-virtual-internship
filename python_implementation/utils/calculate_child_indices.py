2c5ecdef44f45ea24cb8481f8ee5dc7d6158c02a
Ayo <ayooluwaadedipe@gmail.com>
2023-02-11 08:09:33 +0000

    Calculate child indices util

---
+++ python_implementation/utils/calculate_child_indices.py
@@ -0,0 +1,5 @@
+def calculate_child_indices(parent_index, spawn_exponent, heap_array_length):
+  max_num_of_children = 2**spawn_exponent
+  first_child_index = (parent_index*max_num_of_children) - (max_num_of_children-2)
+  last_child_index = (parent_index*max_num_of_children) + 1
+  return [child_index for child_index in range(first_child_index, last_child_index+1) if child_index < heap_array_length]
