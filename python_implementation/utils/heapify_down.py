d14f501c6eede1efff252f39f3f5b81f495bacfa
Ayo <ayooluwaadedipe@gmail.com>
2023-02-11 16:02:03 +0000

    Extract max child value and index into a separate function

--- python_implementation/utils/heapify_down.py
+++ python_implementation/utils/heapify_down.py
@@ -1,14 +1,12 @@
 from utils.calculate_child_indices import calculate_child_indices
 from utils.swap import swap
+from utils.max_child import max_child_value_and_index
 
 def heapify_down(heap_array, parent_index, spawn_exponent):
   child_indices = calculate_child_indices(parent_index, spawn_exponent, len(heap_array))
   if(len(child_indices) < 1):
     return heap_array
-  child_values = list(map(lambda child_index: heap_array[child_index], child_indices))
-
-  max_child_value = max(child_values)
-  max_child_index = child_indices[child_values.index(max_child_value)]
+  [max_child_index, max_child_value] = max_child_value_and_index(heap_array, child_indices)
   if max_child_value > heap_array[parent_index]:
     heap_array = swap(heap_array, max_child_index, parent_index)
   return heapify_down(heap_array, max_child_index, spawn_exponent)
---
+++ python_implementation/utils/max_child.py
@@ -0,0 +1,6 @@
+def max_child_value_and_index(heap_array, child_indices):
+  child_values = list(map(lambda child_index: heap_array[child_index], child_indices))
+
+  max_child_value = max(child_values)
+  max_child_index = child_indices[child_values.index(max_child_value)]
+  return [max_child_index, max_child_value]
