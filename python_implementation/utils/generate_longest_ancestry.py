a6ae58aeb0642f6235a075532ba1e962099549a8
Ayo <ayooluwaadedipe@gmail.com>
2023-02-11 19:57:42 +0000

    Longest ancestry util

--- python_implementation/utils/generate_longest_ancestry.py
+++ python_implementation/utils/generate_longest_ancestry.py
@@ -1,12 +1,20 @@
 from utils.calculate_parent_index import calculate_parent_index
-def generate_longest_ancestry(spawn_exponent, heap_array_length=0, generation=1, longest_ancestry=[], child_index=None):
+from utils.calculate_child_indices import calculate_child_indices
+from utils.max_child import max_child_value_and_index
+def generate_longest_ancestry(spawn_exponent, heap_array, last_child_index=0, generation=1, longest_ancestry=[]):
   """
   Returns a list of indices from the last child to the second generation
   """
-  if child_index in range(2, ((2**spawn_exponent)+2)):
+  if last_child_index in range(2, ((2**spawn_exponent)+2)):
     return longest_ancestry
   if generation == 1:
-    child_index = heap_array_length - 1
-  parent_index = (calculate_parent_index(child_index, spawn_exponent))
+    last_child_index = len(heap_array) - 1
+  parent_index = (calculate_parent_index(last_child_index, spawn_exponent))
+  if generation == 1:
+    #Ensure that the last index in the ancestry holds the highest value amongst its siblings.
+    last_generation_child_indices = calculate_child_indices(parent_index, spawn_exponent, len(heap_array))
+    [max_last_generation_child_index, _] = max_child_value_and_index(heap_array, last_generation_child_indices)
+    longest_ancestry.append(max_last_generation_child_index)
   longest_ancestry.insert(0, parent_index)
-  return generate_longest_ancestry(spawn_exponent, heap_array_length, generation+1, longest_ancestry, parent_index)
+  return generate_longest_ancestry(spawn_exponent, heap_array, parent_index, generation+1, longest_ancestry)
+
