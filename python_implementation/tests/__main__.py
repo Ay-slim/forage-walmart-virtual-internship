8a3c19bac82bc8f9bcbc0fc98d040bfd43a3392e
Ayo <ayooluwaadedipe@gmail.com>
2023-02-15 11:09:46 +0000

    Test update

--- python_implementation/tests/__main__.py
+++ python_implementation/tests/__main__.py
@@ -20,7 +20,7 @@ class TestUtils(unittest.TestCase):
     parent_index6 = calculate_parent_index(21, 3)
     self.assertTrue(parent_index1 == 3 and parent_index2 == 4 and parent_index3 == 5 and parent_index4 == 3 and parent_index5 == 3 and parent_index6 == 3)
   def test_heapify_up(self):
-    test_heapified_array = heapify_up([None, 1, 9, 2, 3], 4, 4, 1)
+    test_heapified_array = heapify_up([None, 1, 9, 2, 3], 4, 3, 1)
     self.assertTrue(test_heapified_array == [None, 3, 9, 2, 1])
   def test_child_indices_generation(self):
     child_indices_power2 = calculate_child_indices(3, 2, 13)
