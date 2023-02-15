37870834f9b9db6ca16573504d04dede468e335b
Ayo <ayooluwaadedipe@gmail.com>
2023-02-15 11:05:56 +0000

    Rewrite heapify_up so we don't gotta pass the same arg twice

--- python_implementation/utils/heapify_up.py
+++ python_implementation/utils/heapify_up.py
@@ -1,6 +1,7 @@
 from utils.calculate_parent_index import calculate_parent_index
 from utils.swap import swap
-def heapify_up(heap_array, current_child_index, child_value_index, spawn_exponent):
+from utils.last_occurence_index import final_index
+def heapify_up(heap_array, current_child_index, new_child_value, spawn_exponent):
   """
   Recursively goes up the heap to ensure that the max heap requirement is satisfied
   - Specifiying child_value_index separately from current_child_index because we may not have to swap on every check
@@ -9,8 +10,7 @@ def heapify_up(heap_array, current_child_index, child_value_index, spawn_exponen
   if current_child_index == 1:
     return heap_array
   parent_index = calculate_parent_index(current_child_index, spawn_exponent)
-  child_value = heap_array[child_value_index]
-  if child_value > heap_array[parent_index]:
-    heap_array = swap(heap_array, child_value_index, parent_index)
-    child_value_index = parent_index
-  return heapify_up(heap_array, parent_index, child_value_index, spawn_exponent)
+  if new_child_value > heap_array[parent_index]:
+    new_child_value_index = final_index(heap_array, new_child_value)
+    heap_array = swap(heap_array, new_child_value_index, parent_index)
+  return heapify_up(heap_array, parent_index, new_child_value, spawn_exponent)
