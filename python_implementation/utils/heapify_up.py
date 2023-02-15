4a665d2555deacf3712f69993fa772d1bafedef7
Ayo <ayooluwaadedipe@gmail.com>
2023-02-15 22:20:42 +0000

    Rewrite heapify up util to allow zero indexing

--- python_implementation/utils/heapify_up.py
+++ python_implementation/utils/heapify_up.py
@@ -7,7 +7,7 @@ def heapify_up(heap_array, current_child_index, new_child_value, spawn_exponent)
   - Specifiying child_value_index separately from current_child_index because we may not have to swap on every check
   - Also, need to pass the specific index as an argument rather than the value because there may be another index holding the same value somewhwere up the heap
   """
-  if current_child_index == 1:
+  if current_child_index == 0:
     return heap_array
   parent_index = calculate_parent_index(current_child_index, spawn_exponent)
   if new_child_value > heap_array[parent_index]:
