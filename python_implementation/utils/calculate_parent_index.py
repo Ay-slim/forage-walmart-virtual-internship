4192e264a76be5550fad84ed159e719cf41f0ded
Ayo <ayooluwaadedipe@gmail.com>
2023-02-15 22:18:09 +0000

    Rewrite parent index util to allow zero indexing

--- python_implementation/utils/calculate_parent_index.py
+++ python_implementation/utils/calculate_parent_index.py
@@ -1,6 +1,5 @@
 def calculate_parent_index(child_index, spawn_exponent):
   max_num_of_children = 2**spawn_exponent
   remainder = child_index % max_num_of_children
-  short_fall = max_num_of_children - remainder
-  divisor = child_index - remainder if remainder in [0, 1] else child_index + short_fall
+  divisor = child_index - max_num_of_children if remainder == 0 else child_index - remainder
   return int(divisor / max_num_of_children)
